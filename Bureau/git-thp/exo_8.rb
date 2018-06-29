c553a6ea13e7425a8d554be42b5b80012c26d182
47fiononana <rantofinonanafanomezana@gmail.com>
2018-06-29 07:16:14 +0300

    Rubs

---
+++ Bureau/git-thp/exo_8.rb
@@ -0,0 +1,4 @@
+puts "Bonjour,quel est ton prenom?"
+print ">"
+user_name = get.chomp
+puts "bonjour , #{user_name}"
---
+++ Bureau/git-thp/exo_9.rb
@@ -0,0 +1,6 @@
+puts "bonjour,quel est votre nom"
+print ">"
+puts"quelle est prénom?"
+print ">"
+user_name = get.chomp
+puts "bonjour, #{name + user_name}" 
